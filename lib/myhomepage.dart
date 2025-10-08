914bb8c129e6541509a75f412fe63af007677069
Gougam <gougammichael@gmail.com>
2025-10-08 16:46:54 +0200

    modif

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomeScreen())));
+  }
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.green,
+        child: FlutterLogo(size: MediaQuery.of(context).size.height));
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Exemple")),
+      body: const Center(
+          child: Text("Welcome to Home Page",
+              style: TextStyle(color: Colors.black, fontSize: 30))),
+    );
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
